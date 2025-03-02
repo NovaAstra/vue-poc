598c4cf6ff0442f60239df1b90676c1ee0e40c4b
NovaAstra <NovaAstra@outlook.com>
2025-03-02 22:18:07 +0800

    main

--- pacakges/molecule/index.tsx
+++ pacakges/molecule/index.tsx
@@ -1,114 +1,42 @@
-import { type VNode, cloneVNode, defineComponent, Fragment, mergeProps, h, ComponentPublicInstance, type AllowedComponentProps, type ComponentCustomProps, type VNodeProps, type ExtractPropTypes, type IntrinsicElementAttributes } from "vue";
+import {
+  type ComponentOptions,
+  type VNode,
+  defineComponent,
+  Fragment,
+  Comment
+} from "vue";
 
-type ElementType = Parameters<typeof h>[0]
+export const createComponent = (
+  name: string,
+  options: ComponentOptions
+) => defineComponent({
+  name,
+  ...options
+})
 
-export interface PolymorphicProps {
-  tag?: boolean
-}
-
-export type AsChildComponent<
-  Component extends ElementType,
-  P extends Record<string, unknown> = Record<never, never>,
-> = {
-  new(): {
-    $props: AllowedComponentProps &
-    ComponentCustomProps &
-    VNodeProps &
-    ExtractPropTypes<Component> &
-    (Component extends keyof IntrinsicElementAttributes
-      ? IntrinsicElementAttributes[Component]
-      : Record<never, never>) &
-    P &
-    PolymorphicProps
-  }
-}
-
-class A {
-  _props: { a: number }
-  public constructor() { }
-}
-
-const a:Te = A
-
-type Te = {
-  new(): {
-    _props: { a: number }
-  }
-}
-
-
-function renderSlotFragments(children?: VNode[]): VNode[] {
+export const renderSlotFragments = (children?: VNode[]) => {
   if (!children) return []
+
   return children.flatMap((child) => {
-    if (child.type === Fragment) return renderSlotFragments(child.children as VNode[])
+    if (child.type === Fragment)
+      return renderSlotFragments(child.children as VNode[])
 
     return [child]
   })
 }
 
-export const Dynamic = defineComponent({
-  name: "Dynamic",
+export const Slot = createComponent('Slot', {
   inheritAttrs: false,
-  setup(_, { attrs, slots }) {
+  setup(_, { slots }) {
     return () => {
-      if (!slots.default) return null
+      if (!slots.default) return null;
 
-      const children = renderSlotFragments(slots.default())
-      console.log(children, "children")
-      const [firstChildren] = children
+      const childrens = renderSlotFragments(slots.default());
 
-      if (Object.keys(attrs).length > 0) {
-        const mergedProps = mergeProps(attrs, {})
-        const cloned = cloneVNode(firstChildren, mergedProps)
+      const firstNonCommentChildrenIndex = childrens.findIndex(child => child.type !== Comment);
+      if (firstNonCommentChildrenIndex === -1) return childrens;
 
-        return cloned;
-      }
-
-      return children;
+      return childrens;
     }
   }
 })
-
-export const TagProps = {
-  tag: {
-    type: Boolean,
-    default: false
-  }
-}
-
-export const withTag = (component: ElementType) => defineComponent({
-  name: 'Polymorphic',
-  inheritAttrs: false,
-  props: TagProps,
-  setup(props, { attrs, slots }) {
-    if (!props.tag) return () => h(component, { ...attrs }, slots.default?.())
-    return () => h(Dynamic, attrs, { default: slots.default })
-  }
-})
-type DOMElements = keyof IntrinsicElementAttributes
-export type HTMLPolymorphicComponents = {
-  [E in DOMElements]: AsChildComponent<E>
-}
-
-export const useFactory = () => {
-  const cache = new Map();
-
-  const factory = new Proxy(withTag, {
-    apply(_, __, [component]) {
-      console.log(2323)
-      return withTag(component)
-    },
-    get(_, element) {
-      const component = element as unknown as ElementType
-      console.log(withTag(component),A)
-      if (!cache.has(component)) {
-        cache.set(component, withTag(component))
-      }
-      return cache.get(component)
-    },
-  }) as unknown as HTMLPolymorphicComponents
-
-  return factory;
-}
-
-export const mol = useFactory()
--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,9 @@
 <script setup lang="ts">
 import { Fragment } from "vue";
-import { mol ,Dynamic} from "../pacakges/molecule"
 </script>
 
 <template>
   <div>
-    <mol.span>
-      <Fragment />
-      <div>12323</div>
-      <div>12323</div>
-      <Dynamic />
-    </mol.span>
+   
   </div>
 </template>
